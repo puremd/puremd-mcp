cfb14fe6c77b3e1cbe8b955ad168d0eb223fb50f
Jason Barry <jason@crawlspace.dev>
2025-04-01 13:32:54 -0700

    Fix bug with build

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { z } from "zod";
-import pkgJson from "../package.json" assert { type: "json" };
+import pkgJson from "../package.json" with { type: "json" };
 
 const { PUREMD_API_KEY = "" } = process.env;
 
